bb37062f2d0c051195896f6cfe5b80eb47c9a532
Ritwik Yadav <ritwikyadav35@gmail.com>
2026-09-22 02:16:50 +0530

    Added a basic backend server

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,20 @@
 import express, { Request, Response } from 'express';
 import {Server} from "socket.io";
 import {createServer} from "http";
+import type {Socket} from "socket.io";
 
 
 const port = process.env.PORT || 3000;
 const server = express();
 
+const httpServer = createServer(server);
+const io = server.listen(5173);
+
+
+io.on('connection', (socket:Socket)=> {
+    console.log("Connected to socket : ", socket.id);
+})
+
+
+httpServer.listen(port)
 
